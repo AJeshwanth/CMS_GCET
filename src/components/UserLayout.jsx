56fd8bf32d5c753d4164eb4075d5c18c9d26847b
AJeshwanth <118911327+AJeshwanth@users.noreply.github.com>
2025-10-27 06:32:47 +0530

    Update UserLayout.jsx

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import '../styles/UserLayout.css';
-import '../styles/navbar.css';
+import '../styles/Navbar.css';
 
 export default function UserLayout({ children }) {
   const role = localStorage.getItem('role') || 'student';
